1a3b8d8fea40319e411a1100ddeff91b383ad647
Daniel Barclay <daniel@urbanintel.io>
2019-01-16 13:36:13 +0000

    Rotate player sprite

--- galactic-battle.py
+++ galactic-battle.py
@@ -2,7 +2,9 @@ import turtle
 
 wn = turtle.Screen()
 wn.bgcolor("#0e202e")
+wn.bgpic("./img/space.gif")
 wn.title("Galactic Battle")
+wn.tracer(0)
 
 
 # classes
@@ -12,15 +14,39 @@ class Player(turtle.Turtle):
         turtle.Turtle.__init__(self)
         self.penup()
         self.shape("triangle")
-        self.color('#e1e5e6')
+        self.color('#97a4a6')
         self.speed = 1
 
     def move(self):
         self.forward(self.speed)
 
+    def turn_left(self):
+        self.left(90)
 
+    def turn_right(self):
+        self.right(90)
+
+    def increase_speed(self):
+        self.speed += 1
+
+    def decrease_speed(self):
+        if self.speed > 1:
+            self.speed -= 1
+
+
+# sprites
+# wn.register_shape("spaceship.gif")
 player = Player()
+# player.shape("spaceship.gif")
+
+# key bindings
+wn.listen()
+wn.onkeypress(player.turn_left, "Left")
+wn.onkeypress(player.turn_right, "Right")
+wn.onkeypress(player.increase_speed, "Up")
+wn.onkeypress(player.decrease_speed, "Down")
 
 # main game loop
 while True:
+    wn.update()
     player.move()
