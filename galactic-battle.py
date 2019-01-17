ec280768380df96966c616ccd309fbd363139610
Daniel Barclay <daniel@urbanintel.io>
2019-01-17 01:12:52 +0000

    Added goal and collision checking

--- galactic-battle.py
+++ galactic-battle.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 wn = turtle.Screen()
 wn.bgcolor("#0e202e")
@@ -33,9 +34,9 @@ class Player(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.penup()
-        self.shape("triangle")
         self.color('#97a4a6')
         self.speed = 1
+        self.shapesize(2, 2, 0)
 
     def move(self):
         self.forward(self.speed)
@@ -59,16 +60,56 @@ class Player(turtle.Turtle):
         if self.speed > 1:
             self.speed -= 1
 
+    def collision_check(self, target):
+        global score
+        if self.distance(target) < 20:
+            score += 1
+            print("Player score: ", score)
+            goal.move_goal()
+
+
+class Goal(turtle.Turtle):
+
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.speed(0)
+        self.color("#ffbb7b")
+        self.speed = 3
+        self.shape("circle")
+        self.setposition(random.randint(-260, 260), random.randint(-260, 260))
+        self.setheading(random.randint(0, 360))
+        self.shapesize(1.15, 1.15)
+
+    def move(self):
+        self.forward(self.speed)
+
+        # border checking
+        if self.xcor() > 275 or self.xcor() < -275:
+            self.left(60)
+        if self.ycor() > 275 or self.ycor() < -275:
+            self.left(60)
+
+    def move_goal(self):
+        self.setposition(random.randint(-260, 260), random.randint(-260, 260))
+        self.setheading(random.randint(0, 360))
+
 
 # sprites
 # wn.register_shape("spaceship.gif")
+
 player = Player()
+
 border = Border()
+
+goal = Goal()
 # player.shape("spaceship.gif")
 
 # draw border
 border.draw_border()
 
+# scoring
+score = 0
 
 # key bindings
 wn.listen()
@@ -80,3 +121,7 @@ wn.onkeypress(player.decrease_speed, "Down")
 # main game loop
 while True:
     player.move()
+    goal.move()
+    # check for collision between player and goal
+    player.collision_check(goal)
+
